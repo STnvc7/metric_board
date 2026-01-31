65625a3af738be838396f103400cca409d73dcaf
hiroh <hirohata0116@gmail.com>
2026-01-31 22:58:27 +0900

    modify

--- metric_board/__init__.py
+++ metric_board/__init__.py
@@ -1,2 +1,2 @@
 from metric_board.evaluator import Evaluator
-from metric_board.interface import MetricBase, MetricOutput, MeanMetric
+from metric_board.interface import MetricBase, MetricOutput, MeanMetric, MSEMetric, MAEMetric, RMSEMetric
